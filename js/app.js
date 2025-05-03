804b861d4037a4742d191b86e55c36a9762db92d
OsmanliFarid <osmanliferid686@gmail.com>
2025-05-03 15:06:51 +0400

    js create

---
+++ js/app.js
@@ -0,0 +1,51 @@
+const QuizBox = document.querySelector("#QuizBox")
+const ENDPOINT = "http://localhost:3000/quiz"
+const cavab = ""
+const QuizShow = (url) =>{
+     axios.get(url).then(({data}) =>{
+        data.options.forEach(element => {
+        });
+        
+        
+        
+        QuizBox.innerHTML += `<h1 class="question">${data.question}</h1>
+                <div class="options-box">
+                    <p class="options" onclick="Option('${data.options[0]}','${data.correctAnswer}')">${data.options[0]}</p>
+                    <p class="options" onclick="Option('${data.options[1]}','${data.correctAnswer}')">${data.options[1]}</p>
+                    <p class="options" onclick="Option('${data.options[2]}','${data.correctAnswer}')">${data.options[2]}</p>
+                    <p class="options" onclick="Option('${data.options[3]}','${data.correctAnswer}')">${data.options[3]}</p>
+                </div>`
+
+        
+        
+        
+     })
+     
+     
+}
+
+
+QuizShow(ENDPOINT)
+const Option = (id,up) =>{
+    
+    const options = document.querySelectorAll(".options")
+    console.log(options);
+    
+        options.forEach((element) => {
+            element.style.backgroundColor = "";
+        });
+       options.forEach((element) =>{
+        
+        if(element.textContent === id){  
+            if(id === up){
+                element.style.backgroundColor = "green"
+            }else{
+                element.style.backgroundColor = "red"
+            }
+        }
+        
+       })
+        
+    }
+    
+
